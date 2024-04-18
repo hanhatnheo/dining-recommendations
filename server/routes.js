48a9d68c01592e95266f816b8e529c236cbfb2c5
Cherilynn Chow <cherilynn0607@gmail.com>
2024-04-18 15:03:15 -0400

    Connect to database and create example route in route.js

---
+++ server/routes.js
@@ -0,0 +1,50 @@
+const mysql = require('mysql')
+const config = require('./config.json')
+
+// Creates MySQL connection using database credential provided in config.json
+// Do not edit. If the connection fails, make sure to check that config.json is filled out correctly
+const connection = mysql.createConnection({
+  host: config.rds_host,
+  user: config.rds_user,
+  password: config.rds_password,
+  port: config.rds_port,
+  database: config.rds_db
+});
+connection.connect((err) => err && console.log(err));
+
+// Example Route: GET /randomrestaurant
+const randomrestaurant = async function(req, res) {
+  // you can use a ternary operator to check the value of request query values
+  // which can be particularly useful for setting the default value of queries
+  // note if users do not provide a value for the query it will be undefined, which is false
+  // const explicit = req.query.explicit === 'true' ? 1 : 0;
+
+  // Here is a complete example of how to query the database in JavaScript.
+  // Only a small change (unrelated to querying) is required for TASK 3 in this route.
+  connection.query(`
+    SELECT *
+    FROM Restaurants
+    LIMIT 1
+  `, (err, data) => {
+    if (err || data.length === 0) {
+      // If there is an error for some reason, or if the query is empty (this should not be possible)
+      // print the error message and return an empty object instead
+      console.log(err);
+      // Be cognizant of the fact we return an empty object {}. For future routes, depending on the
+      // return type you may need to return an empty array [] instead.
+      res.json({});
+    } else {
+      // Here, we return results of the query as an object, keeping only relevant data
+      // being song_id and title which you will add. In this case, there is only one song
+      // so we just directly access the first element of the query results array (data)
+      res.json({
+        name: data[0].name,
+        rating: data[0].stars,
+      });
+    }
+  });
+}
+
+module.exports = {
+  randomrestaurant,
+}
