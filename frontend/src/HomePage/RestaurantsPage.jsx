a8b82aee4d83530511359d710c11af0fd2eecd1a
James Huang <153482468+jameshu88@users.noreply.github.com>
2024-05-02 16:34:07 -0400

    feat: Implemented Search and LazyTable in RestaurantsPage

--- frontend/src/HomePage/RestaurantsPage.jsx
+++ frontend/src/HomePage/RestaurantsPage.jsx
@@ -1,52 +1,16 @@
- import React, { useState } from 'react';
-import GradientSlider from './GradientSlider';
-import LazyTable from './LazyTable';
-
-const RestaurantsPage = () => {
-  // ...
-
-  return (
-    <div>
-      <div>
-        <GradientSlider
-          label="Distance"
-          min={0}
-          max={10}
-          step={0.1}
-          value={5}
-          onChange={(value) => console.log('Distance:', value)}
-        />
-        // add other components 
-      </div>
-      <div>
-        <LazyTable
-          route="/api/restaurants"
-          columns={[
-            { headerName: 'Name', field: 'name' },
-            { headerName: 'Distance', field: 'distance' },
-            { headerName: 'Ratings', field: 'ratings' },
-            { headerName: 'Value', field: 'value' },
-            { headerName: 'Ambiance', field: 'ambiance' },
-            { headerName: 'Food', field: 'food' },
-            { headerName: 'Service', field: 'service' },
-          ]}
-          defaultPageSize={10}
-          rowsPerPageOptions={[5, 10, 25]}
-        />
-      </div>
-    </div>
-  );
-};
-
-export default RestaurantsPage;
-
-/*
-import { useEffect, useState } from 'react';
-import { Button, Checkbox, Container, FormControlLabel, Grid, Link, Slider, TextField } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import {
+  Button,
+  Checkbox,
+  Container,
+  FormControlLabel,
+  Grid,
+  Link,
+  Slider,
+  TextField,
+} from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
-
-import SongCard from '/Users/jamesh/dining-recommendations/frontend/src/HomePage/SongCard.js';
-const config = require('../config.json');
+import config from '/Users/jamesh/dining-recommendations/server/config.json';
 
 export default function SongsPage() {
   const [pageSize, setPageSize] = useState(10);
@@ -55,7 +19,7 @@ export default function SongsPage() {
 
   const [title, setTitle] = useState('');
   const [duration, setDuration] = useState([60, 660]);
-  const [plays, setPlays] = useState([0, 1100000000]);
+  const [plays, setPlays] = useState([0, 1000000000]);
   const [danceability, setDanceability] = useState([0, 1]);
   const [energy, setEnergy] = useState([0, 1]);
   const [valence, setValence] = useState([0, 1]);
@@ -65,7 +29,7 @@ export default function SongsPage() {
     fetch(`http://${config.server_host}:${config.server_port}/search_songs`)
       .then(res => res.json())
       .then(resJson => {
-        const songsWithId = resJson.map((song) => ({ id: song.song_id, ...song }));
+        const songsWithId = resJson.map(song => ({ id: song.song_id, ...song }));
         setData(songsWithId);
       });
   }, []);
@@ -81,27 +45,23 @@ export default function SongsPage() {
     )
       .then(res => res.json())
       .then(resJson => {
-        const songsWithId = resJson.map((song) => ({ id: song.song_id, ...song }));
+        const songsWithId = resJson.map(song => ({ id: song.song_id, ...song }));
         setData(songsWithId);
       });
   }
 
   const columns = [
-    { field: 'title', headerName: 'Title', width: 300, renderCell: (params) => (
-        <Link style={{cursor: 'pointer'}} onClick={() => setSelectedSongId(params.row.song_id)}>{params.value}</Link>
-    ) },
+    { field: 'title', headerName: 'Title', width: 300 },
+    { field: 'duration', headerName: 'Duration', width: 110, valueFormatter: ({ value }) => `${Math.floor(value / 60)}:${value % 60 < 10 ? '0' : ''}${value % 60}` },
     { field: 'plays', headerName: 'Plays', width: 130 },
     { field: 'danceability', headerName: 'Danceability', width: 130 },
     { field: 'energy', headerName: 'Energy', width: 110 },
     { field: 'valence', headerName: 'Valence', width: 110 },
-    { field: 'tempo', headerName: 'Tempo', width: 100 },
-    { field: 'key_mode', headerName: 'Key', width: 100 },
     { field: 'explicit', headerName: 'Explicit', width: 100, renderCell: (params) => params.value ? 'Yes' : 'No' },
   ];
 
   return (
     <Container>
-      {selectedSongId && <SongCard songId={selectedSongId} handleClose={() => setSelectedSongId(null)} />}
       <h2>Search Songs</h2>
       <Grid container spacing={3}>
         <Grid item xs={12}>
@@ -123,6 +83,7 @@ export default function SongsPage() {
             value={duration}
             onChange={(e, newValue) => setDuration(newValue)}
             valueLabelDisplay="auto"
+            valueLabelFormat={value => `${Math.floor(value / 60)}m ${value % 60}s`}
             min={60}
             max={660}
             aria-labelledby="duration-slider"
@@ -130,70 +91,68 @@ export default function SongsPage() {
           <p>Duration Range</p>
         </Grid>
         <Grid item xs={4}>
-  <Slider
-    value={plays}
-    onChange={(e, newValue) => setPlays(newValue)}
-    valueLabelDisplay="auto"
-    valueLabelFormat={(value) => `${(value / 1000000).toFixed(1)}M`}
-    min={0}
-    max={1100000000}
-    aria-labelledby="plays-slider"
-  />
-  <p>Plays Range (Millions)</p>
-</Grid>
-<Grid item xs={4}>
-  <Slider
-    value={danceability}
-    onChange={(e, newValue) => setDanceability(newValue)}
-    valueLabelDisplay="auto"
-    min={0}
-    max={1}
-    step={0.1}
-    aria-labelledby="danceability-slider"
-  />
-  <p>Danceability</p>
-</Grid>
-<Grid item xs={4}>
-  <Slider
-    value={energy}
-    onChange={(e, newValue) => setEnergy(newValue)}
-    valueLabelDisplay="auto"
-    min={0}
-    max={1}
-    step={0.1}
-    aria-labelledby="energy-slider"
-  />
-  <p>Energy</p>
-</Grid>
-<Grid item xs={4}>
-  <Slider
-    value={valence}
-    onChange={(e, newValue) => setValence(newValue)}
-    valueLabelDisplay="auto"
-    min={0}
-    max={1}
-    step={0.1}
-    aria-labelledby="valence-slider"
-  />
-  <p>Valence</p>
-</Grid>
-<Grid item xs={12}>
-  <Button variant="contained" onClick={search} style={{ marginTop: 20 }}>
-    Search
-  </Button>
-</Grid>
-</Grid>
-<h2>Results</h2>
-<DataGrid
-  rows={data}
-  columns={columns}
-  pageSize={pageSize}
-  onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
-  rowsPerPageOptions={[5, 10, 25]}
-  autoHeight
-/>
-</Container>
-);
+          <Slider
+            value={plays}
+            onChange={(e, newValue) => setPlays(newValue)}
+            valueLabelDisplay="auto"
+            valueLabelFormat={(value) => `${(value / 1000000).toFixed(1)}M`}
+            min={0}
+            max={1100000000}
+            aria-labelledby="plays-slider"
+          />
+          <p>Plays Range (Millions)</p>
+        </Grid>
+        <Grid item xs={4}>
+          <Slider
+            value={danceability}
+            onChange={(e, newValue) => setDanceability(newValue)}
+            valueLabelDisplay="auto"
+            min={0}
+            max={1}
+            step={0.1}
+            aria-labelledby="danceability-slider"
+          />
+          <p>Danceability</p>
+        </Grid>
+        <Grid item xs={4}>
+          <Slider
+            value={energy}
+            onChange={(e, newValue) => setEnergy(newValue)}
+            valueLabelDisplay="auto"
+            min={0}
+            max={1}
+            step={0.1}
+            aria-labelledby="energy-slider"
+          />
+          <p>Energy</p>
+        </Grid>
+        <Grid item xs={4}>
+          <Slider
+            value={valence}
+            onChange={(e, newValue) => setValence(newValue)}
+            valueLabelDisplay="auto"
+            min={0}
+            max={1}
+            step={0.1}
+            aria-labelledby="valence-slider"
+          />
+          <p>Valence</p>
+        </Grid>
+        <Grid item xs={12}>
+          <Button variant="contained" onClick={search} style={{ marginTop: 20 }}>
+            Search
+          </Button>
+        </Grid>
+      </Grid>
+      <h2>Results</h2>
+      <DataGrid
+        rows={data}
+        columns={columns}
+        pageSize={pageSize}
+        onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+        rowsPerPageOptions={[5, 10, 25]}
+        autoHeight
+      />
+    </Container>
+  );
 }
-*/
-
